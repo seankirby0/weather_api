31669436231be71a8e655c40c8e5265fd5792b94
seankirby0 <sjkirby0@gmail.com>
2021-08-16 20:13:05 -0500

    some updates

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,10 @@
 console.log('Weather Test')
-
+// key = 53a357ab10f1b78bbb90b2efe22f944e
 
 // Create a getData function
 const getData = async (cityName) => {
 
-    const apiKey = `53a357ab10f1b78bbb90b2efe22f944e`
+    const apiKey = ``
     const searchURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
     let response = await fetch(searchURL);
     return response.json();
@@ -24,17 +24,19 @@ const loadData = async (e) => {
 
 const createWeatherHTML = (localWeather) => {
     const temp = localWeather.main.temp
-    // const tempMax = localWeather.main.temp_max
-    // const tempMin = localWeather.main.temp_min
+    const high = localWeather.main.temp_max
+    const low = localWeather.main.temp_min
     const weather = localWeather.weather[0].description
     const city = localWeather.name
 
 
-    console.log(temp, weather);
+    console.log(temp, weather, city, high, low);
     let tempDisplay = document.getElementById('localWeatherDisplay');
     let myTemp = document.getElementById('temp');
     let myWeather = document.getElementById('weather');
     let myCity = document.getElementById('city')
+    let myTempMax = document.getElementById('high')
+    let myTempMin = document.getElementById('low')
 
     
     // Display data in HTML
@@ -46,9 +48,11 @@ const createWeatherHTML = (localWeather) => {
     // let htmlWeather = document.createElement('h3');
     myWeather.innerHTML = weather;
     // htmlWeather.innerHTML = weather;
-    // tempDisplay.insertAdjacentElement('beforeend', htmlWeather)
-        
+    // tempDisplay.insertAdjacentElement('beforeend', htmlWeather)       
     myCity.innerHTML = city;
+    myTempMax.innerHTML = high;
+    myTempMin.innerHTML = low;
+
 }
 
 
